95b923b8f7124bda37835ec44f29a069685dac12
Tristan Phease <tristan.phease@gmail.com>
2025-12-06 23:26:07 +1300

    Add solution for part 2 for day6 in f# (this one sucks)

--- day6/Program.fs
+++ day6/Program.fs
@@ -42,20 +42,60 @@ let convertInput (fileLines: seq<string>) : seq<Calculations> =
                 match z with
                     | "*" -> accum |> fst, Some Multiply
                     | "+" -> accum |> fst, Some Add
-                    | _ -> failwith "Invalid multiplier"
+                    | _ -> failwith "Invalid operator"
     ))
     |> Seq.map(fun x -> {
         numbers = x |> fst;
         operator = x |> snd |> Option.get
     })
 
+
+// this is awful, not functional at all
+// but my brain isn't working well enough to work it out
+let convertInputPart2 (fileLines: seq<string>) : seq<Calculations> = 
+    // iterate through list of strings one char at a time 
+    let lineLength = fileLines |> Seq.item 0 |> String.length
+    let mutable currentNumbers = []
+    let mutable currentOperator = None
+    let mutable calcs = []
+    for i in 0..lineLength - 1 do
+        let mutable number: char list = []
+        for line in fileLines do
+            match line.[i] with 
+                | '*' -> currentOperator <- Some Multiply
+                | '+' -> currentOperator <- Some Add
+                | ' ' -> ()
+                | a -> number <- number @ [a]
+        if number |> List.isEmpty then
+            let newCalc = {
+                numbers = currentNumbers;
+                operator = currentOperator |> Option.get
+            }
+            calcs <- calcs @ [newCalc]
+            currentNumbers <- []
+            currentOperator <- None
+        else
+            let newNumber = number |> List.map Char.ToString |> String.concat "" |> Int64.Parse
+            currentNumbers <- currentNumbers @ [newNumber]
+    let newCalc = {
+        numbers = currentNumbers;
+        operator = currentOperator |> Option.get
+    }
+    calcs @ [newCalc]
+
+
 [<EntryPoint>]
 let main args =
-    if args.Length > 0 then
+    if args.Length > 1 && args[1] = "2" then 
+        let inputText = File.ReadLines args[0]
+        let calculations = convertInputPart2 inputText 
+        // calculations |> Seq.iter printCalc
+        let value = calculations |> Seq.map calcValue |> Seq.fold(fun x y -> x + y) 0L
+        printfn "Total value is %d" value
+    elif args.Length > 0 then
         let inputText = File.ReadLines args[0]
         let calculations = convertInput inputText 
-        // calculations |> Seq.iter (fun x -> printfn "value = %d" (calcValue x)) 
-        calculations |> Seq.iter printCalc
+        // calculations |> Seq.iter printCalc
         let value = calculations |> Seq.map calcValue |> Seq.fold(fun x y -> x + y) 0L
         printfn "Total value is %d" value
     else
